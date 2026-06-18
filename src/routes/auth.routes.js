94fcbd3d6023a825b103f2d871c349de506714c2
Devam <devamdp27@gmail.com>
2026-06-18 23:43:04 +0530

    Register and Login apis done

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,21 @@
+const {Router} = require("express")
+const authController = require("../controller/auth.controller")
+
+const authRouter = Router()
+
+/**
+ * @route POST /api/auth/register
+ * @description Register a new user
+ * @access Public
+ */
+authRouter.post("/register", authController.registerUserController)
+
+
+/**
+ * @route POST /api/auth/login
+ * @description Login an existing user
+ * @access Public
+ */
+authRouter.post("/login", authController.loginUserController)
+
+module.exports = authRouter
